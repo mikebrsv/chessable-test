629cc34d650f191da6ded2b6640f036ed0f14726
Mikhail Borisov <mikebrsv@gmail.com>
2021-03-24 18:01:44 +0100

    Board.js refactored, introduce Pawn.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,87 +1,32 @@
 import React from 'react'
 import Square from './Square';
+import Pawn from './Pawn'; 
 
 const Board = () => {
-  const renderSquare = (color) => {
-    return (
-      <Square color={color} />
-    )
+  const renderSquare = (index) => {
+    let color;
+
+    if (
+      (index >= 0 && index <= 7)
+      || (index >= 16 && index <= 23)
+      || (index >= 32 && index <= 39)
+      || (index >= 48 && index <= 55)
+    ) {
+      index % 2 === 0 ? color = 'white' : color = 'black'
+    } else {
+      index % 2 === 0 ? color = 'black' : color = 'white'
+    }
+
+    return <Square key={index} color={color} index={index} />
   }
 
   return (
-    <div className="board">
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-      {renderSquare('black')}
-      {renderSquare('white')}
-    </div>
+    <>
+      <Pawn />
+      <div className="board">
+        {[...Array(64)].map((_, index) => renderSquare(index))}
+      </div>
+    </>
   )
 }
 
---
+++ src/components/Pawn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChessPawn } from '@fortawesome/free-solid-svg-icons'
+
+const Pawn = () => {
+  return (
+    <FontAwesomeIcon icon={faChessPawn} className="pawn" />
+  )
+}
+
+export default Pawn
